98eca1444c0a7b7206b099d386837429fac0d977
Peter Biro <github@vlx.dk>
2021-06-03 08:22:47 +0200

    Init

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,50 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import styled from '@emotion/styled'
+import { AppBar, AppBarProvider, AppBarItem } from "@dfds-ui/react-components";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const StyledAppBar = styled(AppBar)`
+  button {
+    display: none;
+  }
+`
+
+const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
+
+    <AppBarProvider>
+      <StyledAppBar>
         <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
+          <a><AppBarItem title="Home" id="home" /></a>
+        </Link>
+
         <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
+          <a><AppBarItem title="About" id="about" /></a>
+        </Link>
+
         <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
+          <a><AppBarItem title="User list" id="userList" /></a>
+        </Link>
+      </StyledAppBar>
+    </AppBarProvider>
+
     {children}
     <footer>
       <hr />
       <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
---
+++ next.config.js
@@ -0,0 +1,56 @@
+const withPlugins = require("next-compose-plugins");
+const withTranspileModules = require("next-transpile-modules");
+
+module.exports = withPlugins(
+  [
+    withTranspileModules([
+      "@dfds-ui/react-components",
+      "@dfds-ui/colors",
+      "@dfds-ui/icons",
+      "@dfds-ui/theme",
+      "@dfds-ui/typography",
+      "@dfds-ui/modal",
+      "@dfds-ui/grid",
+      "@dfds-platform/business-components",
+      "@dfds-ui/experiences",
+    ]),
+  ],
+  {
+    trailingSlash: true,
+    future: {
+      webpack5: true,
+      webpack(config, options) {
+
+
+        config.module.rules.push({
+          test: /\.test.js$/,
+          loader: 'ignore-loader',
+        })
+
+        config.module.rules.push({
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        })
+
+        config.module.rules.push({
+          test: /\.(eot|woff|woff2)$/,
+          loader: 'ignore-loader',
+          // use: {
+          //   loader: 'url-@dfds-ui',
+          //   options: {
+          //     limit: 10000,
+          //     name: '[name].[ext]',
+          //   },
+          // },
+        })
+
+
+
+        return config
+      },
+    },
+    images: {
+      domains: [],
+    },
+  }
+);
